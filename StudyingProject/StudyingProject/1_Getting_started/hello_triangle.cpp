fc220771da4323f3586cdc856b08e4dedf916048
Darijan Mićić <darijan.micic10@gmail.com>
2024-07-24 18:39:53 +0200

    feat(2.4): copy hello window as basis for triangle
    Getting Started -> Hello Triangle

---
+++ StudyingProject/StudyingProject/1_Getting_started/hello_triangle.cpp
@@ -0,0 +1,83 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+const int window_width = 800;
+const int window_height = 600;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+int main()
+{
+	// Initialize the GLFW library.
+	if (!glfwInit())
+	{
+		std::cout << "GLFW library failed to initialize!";
+
+		return 1;
+	}
+	// Specify the OpenGL version and profile.
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Create a window and make the context of created window the main context on the current thread.
+	GLFWwindow* window = glfwCreateWindow(window_width, window_height, "StudyingProject", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Window was not created!";
+		glfwTerminate();
+
+		return 2;
+	}
+	glfwMakeContextCurrent(window);
+
+	// Register the callback functions after the window is created and before the render loop is started.
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// Initialize the GLAD library.
+	if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress))
+	{
+		std::cout << "GLAD library failed to initialize!";
+		glfwTerminate();
+
+		return 3;
+	}
+
+	// Rendering loop.
+	while (!glfwWindowShouldClose(window))
+	{
+		// First part: Process the user's input.
+		processInput(window);
+
+		// Second part: Rendering commands.
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Third part: Swap buffers, check for events and call the events if they occured.
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	// Terminate the GLFW library, which frees up all allocated resources.
+	glfwTerminate();
+
+	return 0;
+}
+
+// Callback function.
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+// Input processing function.
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
+}
