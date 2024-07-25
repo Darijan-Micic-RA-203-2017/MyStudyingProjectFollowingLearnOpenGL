c5ebb3e3a53a9d75d57dd563becc864fe6d04573
Darijan Mićić <darijan.micic10@gmail.com>
2024-07-25 14:53:46 +0200

    feat(2.4): tell gl how to interpret vertex data
    Getting Started -> Hello Triangle -> Linking Vertex Attributes

--- StudyingProject/StudyingProject/1_Getting_started/hello_triangle.cpp
+++ StudyingProject/StudyingProject/1_Getting_started/hello_triangle.cpp
@@ -144,6 +144,11 @@ int main()
 	// Vertex data is now stored on the graphics card's memory.
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	// Tell OpenGL how it should interpret vertex data, per vertex attribute.
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) 0);
+	// Enable vertex attribute.
+	glEnableVertexAttribArray(0);
+
 	// Rendering loop.
 	while (!glfwWindowShouldClose(window))
 	{
