e1e43cb5021ab2c696fabe0b3af5eba33f5e1250
Darijan Mićić <darijan.micic10@gmail.com>
2024-07-25 15:28:01 +0200

    feat(2.4): use vertex array object to store vbos
    Getting Started -> Hello Triangle -> Vertex Array Object

--- StudyingProject/StudyingProject/1_Getting_started/hello_triangle.cpp
+++ StudyingProject/StudyingProject/1_Getting_started/hello_triangle.cpp
@@ -131,10 +131,14 @@ int main()
 	};
 
 	// Create memory on the GPU where vertex data will be stored.
-	// Vertex buffer object will be used to handle said data.
+	// Said data will be handled by vertex array object and vertex buffer objects inside that VAO.
+	// Core OpenGL REQUIRES the use of VAOs!
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
 	unsigned int VBO;
 	glGenBuffers(1, &VBO);
 	// Bind (assign) the newly created VBO to OpenGL's context.
+	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	// Copy user-defined data into the currently bound buffer.
 	// Vertex data is now stored on the graphics card's memory.
@@ -144,6 +148,10 @@ int main()
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) 0);
 	// Enable vertex attribute.
 	glEnableVertexAttribArray(0);
+
+	// Unbind VBO and VAO for safety reasons. This is not neccessary.
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
 
 	// Rendering loop.
 	while (!glfwWindowShouldClose(window))
@@ -165,6 +173,7 @@ int main()
 
 	// De-allocate all resources once they're no longer needed.
 	glDeleteBuffers(1, &VBO);
+	glDeleteVertexArrays(1, &VAO);
 	glDeleteProgram(shaderProgram);
 
 	// Terminate the GLFW library, which frees up all allocated resources.
