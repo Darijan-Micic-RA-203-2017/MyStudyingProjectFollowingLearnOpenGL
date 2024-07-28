949a4c86443a88391b674fdbd9d1a1c0e8495efb
Darijan Mićić <darijan.micic10@gmail.com>
2024-07-28 15:11:06 +0200

    improvement(2.4): keep main app running with loop
    Getting Started -> Hello Triangle -> Exercises

--- StudyingProject/StudyingProject/main.cpp
+++ StudyingProject/StudyingProject/main.cpp
@@ -8,32 +8,36 @@
 int main()
 {
 	int argument = 0;
-	std::cout << "Enter argument: ";
-	std::cin >> argument;
-
-	switch (argument)
+	do
 	{
-		case 1:
-			draw_hello_window();
-			break;
-		case 2:
-			draw_hello_triangle();
-			break;
-		case 3:
-			draw_hello_triangle_indexed();
-			break;
-		case 4:
-			draw_hello_triangle_exercise1();
-			break;
-		case 5:
-			draw_hello_triangle_exercise2();
-			break;
-		case 6:
-			draw_hello_triangle_exercise3();
-			break;
-		default:
-			break;
+		std::cout << "Enter argument: ";
+		std::cin >> argument;
+
+		switch (argument)
+		{
+			case 1:
+				draw_hello_window();
+				break;
+			case 2:
+				draw_hello_triangle();
+				break;
+			case 3:
+				draw_hello_triangle_indexed();
+				break;
+			case 4:
+				draw_hello_triangle_exercise1();
+				break;
+			case 5:
+				draw_hello_triangle_exercise2();
+				break;
+			case 6:
+				draw_hello_triangle_exercise3();
+				break;
+			default:
+				break;
+		}
 	}
+	while (argument != -1);
 
 	return 0;
 }
