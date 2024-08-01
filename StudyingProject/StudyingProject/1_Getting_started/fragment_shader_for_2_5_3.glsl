681527722beed9fafde2dd9b31e38db816ac96d5
Darijan Mićić <darijan.micic10@gmail.com>
2024-08-02 00:39:40 +0200

    improvement(2.5): do exercise 3
    Getting Started -> Shaders -> Exercises
    
    I sent the vertex position to the fragment shader using the out keyword. Then I set the fragment's color equal to this vertex position. The triangle has its bottom left angle black colored, bottom right angle red colored and top angle green colored. Vertex position values are interpolated across the triangle.
    
    Bottom left angle and area diagonally spread across triangle's center is black because it's x-coordinate and y-coordinate are in range [-0.5f, 0.0f]. Negative values get clamped to a value of 0.0f, which represents black color.

--- StudyingProject/StudyingProject/1_Getting_started/fragment_shader_for_2_5_3.glsl
+++ StudyingProject/StudyingProject/1_Getting_started/fragment_shader_for_2_5_3.glsl
@@ -1,10 +1,12 @@
 #version 330 core
 
-in vec4 ourColor;
+// in vec4 ourColor;
+in vec3 ourPosition;
 
 out vec4 FragColor;
 
 void main()
 {
-    FragColor = ourColor;
+    // FragColor = ourColor;
+    FragColor = vec4(ourPosition, 1.0f);
 }
--- StudyingProject/StudyingProject/1_Getting_started/vertex_shader_for_2_5_3.glsl
+++ StudyingProject/StudyingProject/1_Getting_started/vertex_shader_for_2_5_3.glsl
@@ -3,11 +3,13 @@
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec4 aColor;
 
-out vec4 ourColor;
+// out vec4 ourColor;
+out vec3 ourPosition;
 
 void main()
 {
 	// GLSL allows passing vectors as arguments to different vectors constructor calls.
 	gl_Position = vec4(aPos, 1.0f);
-	ourColor = aColor;
+	// ourColor = aColor;
+	ourPosition = aPos;
 }
