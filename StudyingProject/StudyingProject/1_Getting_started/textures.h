296b99e1412897ba06b1bbfab14bc2cbd94a6604
Darijan Mićić <darijan.micic10@gmail.com>
2024-08-02 20:16:14 +0200

    chore(2.6): copy indexed triangle as textures base
    Getting Started -> Textures
    
    I also created an additional filter (logical folder) in "StudyingProject", called "Shader Files".
    Reference: https://learn.microsoft.com/en-us/cpp/build/reference/vcxproj-filters-files?view=msvc-170

---
+++ StudyingProject/StudyingProject/1_Getting_started/textures.h
@@ -0,0 +1,7 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int draw_textures();
+void framebuffer_size_callback_for_textures(GLFWwindow* window, int width, int height);
+void processInput_for_textures(GLFWwindow* window);
