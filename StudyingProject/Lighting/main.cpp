e165f6a3d62873101890523f939f3afe99e5b045
Darijan Mićić <darijan.micic10@gmail.com>
2024-09-08 22:12:55 +0200

    chore(3.2): copy exercise 2 as exercise 3 basis
    Lighting -> Basic Lighting -> Exercises

--- StudyingProject/Lighting/main.cpp
+++ StudyingProject/Lighting/main.cpp
@@ -2,6 +2,7 @@
 #include "Basic_Lighting/basic_lighting_diffuse.h"
 #include "Basic_Lighting/basic_lighting_specular.h"
 #include "Basic_Lighting/basic_lighting_exercise2.h"
+#include "Basic_Lighting/basic_lighting_exercise3.h"
 
 int main()
 {
@@ -10,7 +11,7 @@ int main()
 	int argument = 0;
 	while (argument != -1)
 	{
-		std::cout << "Enter argument from set {1, ..., 4}: ";
+		std::cout << "Enter argument from set {1, ..., 5}: ";
 		std::cin >> argument;
 
 		switch (argument)
@@ -32,6 +33,11 @@ int main()
 			break;
 		case 4:
 			returnValue = draw_basic_lighting_exercise2();
+			std::cout << "\"Lighting - Basic Lighting, exercise 2\" exited with return value " 
+				<< returnValue << ".\n" << std::endl;
+			break;
+		case 5:
+			returnValue = draw_basic_lighting_exercise3();
 			std::cout << "\"Lighting - Basic Lighting, exercise 2\" exited with return value " 
 				<< returnValue << ".\n" << std::endl;
 			break;
