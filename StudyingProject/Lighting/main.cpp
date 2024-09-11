1eacf163d56ce86ccbc336547003c2f3762613cf
Darijan Mićić <darijan.micic10@gmail.com>
2024-09-12 00:18:49 +0200

    chore(3.4): copy materials as lighting maps basis
    Lighting -> Lighting maps -> Diffuse maps

--- StudyingProject/Lighting/main.cpp
+++ StudyingProject/Lighting/main.cpp
@@ -5,6 +5,7 @@
 #include "Basic_Lighting/basic_lighting_exercise3.h"
 #include "Materials/materials.h"
 #include "Materials/materials_exercise1.h"
+#include "Lighting_maps/lighting_maps_diffuse.h"
 
 int main()
 {
@@ -13,7 +14,7 @@ int main()
 	int argument = 0;
 	while (argument != -1)
 	{
-		std::cout << "Enter argument from set {1, ..., 7}: ";
+		std::cout << "Enter argument from set {1, ..., 8}: ";
 		std::cin >> argument;
 
 		switch (argument)
@@ -51,6 +52,11 @@ int main()
 		case 7:
 			returnValue = draw_materials_exercise1();
 			std::cout << "\"Lighting - Materials, exercise 1\" exited with return value " 
+				<< returnValue << ".\n" << std::endl;
+			break;
+		case 8:
+			returnValue = draw_lighting_maps_diffuse();
+			std::cout << "\"Lighting - Lighting maps, diffuse\" exited with return value " 
 				<< returnValue << ".\n" << std::endl;
 			break;
 		default:
