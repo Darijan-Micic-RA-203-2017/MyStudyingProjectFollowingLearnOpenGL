f21ab5a56f249cfc74047cb1cab4bdf7f1ce557d
Darijan Mićić <darijan.micic10@gmail.com>
2024-09-25 19:56:22 +0200

    feat(4): load model into scene & get model's dir.
    Model Loading -> Model -> Importing a 3D model into OpenGL

--- StudyingProject/Model_Loading/model.h
+++ StudyingProject/Model_Loading/model.h
@@ -4,6 +4,9 @@
 #ifndef MODEL_H
 #define MODEL_H
 
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
 #include <string>
 #include <vector>
 using namespace std;
@@ -16,7 +19,32 @@ private:
 	vector<Mesh> meshes;
 	string directory;
 
-	void loadModelFrom(const char* path);
+	// Use the Assimp library to load the model into the Assimp's data structure. The root object of the Assimp's
+	// data structure is the "SCENE" object. Once we have the scene, we can access ALL THE DATA we need from the
+	// loaded model.
+	void loadModelFrom(string path)
+	{
+		Assimp::Importer modelImporter;
+		// "ReadFile" method of Assimp's "Importer" class expects post-processing options as its second argument.
+		// "aiProcess_Triangulate" - tells Assimp that if the model doesn't entirely consist of triangles, it
+		// should transform all the model's primitive shapes to triangles first.
+		// "aiProcess_FlipUVs"     - tells Assimp to flip the texture coordinates on the y-axis where necessary.
+		// This option is basically a replacement for using "stb_image" library's
+		// "stbi_set_flip_vertically_on_load(true)" method (in "Getting_Started" and "Lighting" projects).
+		const aiScene* scene = modelImporter.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs);
+		if (!scene || !scene->mRootNode || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE)
+		{
+			cout << "An error occured while loading the model!\n" << modelImporter.GetErrorString() << endl;
+
+			return;
+		}
+
+		// Retrieve the path of directory containing the model.
+		directory = path.substr(0, path.find_last_of('/'));
+
+		processNode(scene->mRootNode, scene);
+	}
+
 	void processNode(aiNode* node, const aiScene* scene);
 	Mesh processMesh(aiMesh* mesh, const aiScene* scene);
 	vector<Texture> loadTexturesOfMaterial(aiMaterial* material, aiTextureType type, string nameOfType);
