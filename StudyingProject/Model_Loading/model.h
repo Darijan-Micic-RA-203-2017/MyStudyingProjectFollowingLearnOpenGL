d7d8e25cd55ae15e6179934292651c4978ef441c
Darijan Mićić <darijan.micic10@gmail.com>
2024-09-26 18:21:17 +0200

    feat(4): translate assimp's mesh material
    Model Loading -> Model -> Importing a 3D model into OpenGL -> Material

--- StudyingProject/Model_Loading/model.h
+++ StudyingProject/Model_Loading/model.h
@@ -4,6 +4,7 @@
 #ifndef MODEL_H
 #define MODEL_H
 
+#include <glad/glad.h>
 #include <assimp/Importer.hpp>
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
@@ -11,6 +12,7 @@
 #include <vector>
 using namespace std;
 #include "shader_program.h"
+#include "stb_image.h"
 #include "mesh.h"
 
 class Model
@@ -118,12 +120,140 @@ private:
 			}
 		}
 
+		// Translate "aiMaterial" object to a vector of our own "Texture" structures.
 		vector<Texture> textures;
+		unsigned int indexOfMeshMaterial = mesh->mMaterialIndex;
+		// Model may not have a material (consisting of textures) at all, so that's why an "if"-branch is needed.
+		if (indexOfMeshMaterial)
+		{
+			aiMaterial* meshMaterial = scene->mMaterials[indexOfMeshMaterial];
+
+			vector<Texture> diffuseMaps = 
+				loadTexturesOfMaterial(meshMaterial, aiTextureType_DIFFUSE, "texture_diffuse");
+			// Insert all diffuse maps (from the beginning to the end of the returned vector) to the end of the
+			// textures vector.
+			textures.insert(textures.end(), diffuseMaps.begin(), diffuseMaps.end());
+
+			vector<Texture> specularMaps = 
+				loadTexturesOfMaterial(meshMaterial, aiTextureType_SPECULAR, "texture_specular");
+			// Insert all specular maps (from the beginning to the end of the returned vector) to the end of the
+			// textures vector.
+			textures.insert(textures.end(), specularMaps.begin(), specularMaps.end());
+
+			vector<Texture> emissionMaps = 
+				loadTexturesOfMaterial(meshMaterial, aiTextureType_EMISSIVE, "texture_emission");
+			// Insert all emission maps (from the beginning to the end of the returned vector) to the end of the
+			// textures vector.
+			textures.insert(textures.end(), emissionMaps.begin(), emissionMaps.end());
+		}
 
 		return Mesh(vertices, indices, textures);
 	}
 
-	vector<Texture> loadTexturesOfMaterial(aiMaterial* material, aiTextureType type, string nameOfType);
+	// Retrieve, load and initialize the textures of the material. Function returns a vector of textures that we
+	// insert at the end of the model's textures vector.
+	// NOTE: We make the assumption that texture file paths in model files are local to the actual model object,
+	// e.g. in the same directory as the location of the model itself. We can then simply concatenate the texture
+	// location string and the directory string we retrieved earlier (in the "loadModelFrom" function) to get the
+	// complete texture path (that's why the "loadTextureFromFile" function also needs the directory string).
+	// Some models found on the internet use absolute paths for their texture locations, which won't work on each
+	// machine. In that case you probably want to manually edit the file to use local paths for the textures
+	// (if possible). 
+	vector<Texture> loadTexturesOfMaterial(aiMaterial* material, aiTextureType type, string nameOfType)
+	{
+		// Iterate over all the texture locations of the given type of texture.
+		vector<Texture> textures;
+		for (unsigned int i = 0u; i < material->GetTextureCount(type); i++)
+		{
+			// Retrieve the location of the texture file.
+			aiString str;
+			material->GetTexture(type, i, &str);
+
+			// Load the texture from file.
+			Texture texture;
+			texture.id = loadTextureFromFile(str.C_Str(), directory);
+			texture.type = nameOfType;
+			texture.path = str.C_Str();
+
+			textures.push_back(texture);
+		}
+
+		return textures;
+	}
+
+	// Load the texture image file, create the texture, set texture wrapping and filtering parameters and finally
+	// generate the texture and all its required mipmaps.
+	unsigned int loadTextureFromFile(const char* path, const string& directory)
+	{
+		// Create memory on the GPU where texture will be stored.
+		unsigned int texture;
+		glGenTextures(1, &texture);
+		// Bind (assign) the newly created texture to OpenGL's context.
+		glBindTexture(GL_TEXTURE_2D, texture);
+
+		// Set texture wrapping parameters. Texture coordinates are in range [0.0f, 1.0f]. If texture coordinates
+		// are specified outside of mentioned range, texture wrapping option determines the look.
+		// Each texture wrapping option can be set per coordinate axis (s, t and r if 3D textures are used).
+		// s-axis, t-axis and r-axis correspond to x-axis, y-axis and z-axis, respectively.
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+		// Set texture filtering parameters. Texture coordinates do not depend on resolution, but can be any
+		// floating point value. Therefore, OpenGL needs to figure out which texture pixel (texel) to map the
+		// texture coordinate to. Nearest neighbour filtering is better suited for minifying operations, while
+		// (bi)linear filtering is better suited for magnifying operations.
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		// Mipmaps are used to improve minifying, not magnifying. Setting one of the mipmap filtering options as
+		// the magnification filter will generate the OpenGL "GL_INVALID_ENUM" error code.
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		// Form the filename by concatenation.
+		string filename = string(path);
+		filename = directory + "/" + filename;
+		// Load the image that will be used as a texture.
+		int textureImageWidth, textureImageHeight, numberOfColorChannelsInTextureImage;
+		unsigned char* pixels = stbi_load(filename.c_str(), &textureImageWidth, &textureImageHeight, 
+			&numberOfColorChannelsInTextureImage, 0);
+		if (pixels)
+		{
+			// Determine image format from number of color channels in texture image.
+			GLenum format = 0u;
+			switch (numberOfColorChannelsInTextureImage)
+			{
+			case 1:
+				format = GL_RED;
+				break;
+			case 3:
+				format = GL_RGB;
+				break;
+			case 4:
+				format = GL_RGBA;
+				break;
+			default:
+				stbi_image_free(pixels);
+				glBindTexture(GL_TEXTURE_2D, 0u);
+				glDeleteTextures(1, &texture);
+
+				return;
+			}
+
+			// Generate a texture using the previously loaded image data (pixels).
+			glTexImage2D(GL_TEXTURE_2D, 0, format, textureImageWidth, textureImageHeight, 0, format, 
+				GL_UNSIGNED_BYTE, pixels);
+			// Automatically generate all the required mipmaps for the currently bound texture.
+			glGenerateMipmap(GL_TEXTURE_2D);
+		}
+		else
+		{
+			cout << "Image of would-be-texture could not be loaded!" << std::endl;
+			glBindTexture(GL_TEXTURE_2D, 0u);
+			glDeleteTextures(1, &texture);
+		}
+		// Free the image memory.
+		stbi_image_free(pixels);
+
+		return texture;
+	}
 public:
 	Model(const char* path)
 	{
