8bd602f9f8a6a71d892197feae2fd313a0110192
Darijan Mićić <darijan.micic10@gmail.com>
2024-10-31 15:14:41 +0100

    feat: do practice 3 - task 3 (letter D)
    Zadatak 3
    Napisati program koji crta šarenu traku proizvoljnih boja oko ivica prozora koji je dimenzija 500x500, a na sredini ekrana prvo slovo svog imena proizvoljne boje na svetlo sivoj pozadini. Širina trake iznosi 30 % širine jednog kvadranta. Debljina linija slova mora biti veća od 1 piksela. Boja pozadine se tokom vremena smenjuje od potpuno bele do potpuno crne boje.

--- StudyingProject/RG_vezbe/Vezbe_03/Zadatak_03/vezbe_03_zadatak_03.cpp
+++ StudyingProject/RG_vezbe/Vezbe_03/Zadatak_03/vezbe_03_zadatak_03.cpp
@@ -1,3 +1,8 @@
+#define NUMBER_OF_DATA_IN_VERTEX_OF_CIRCLE 5u
+// Number of discrete points the circle consists of. The circle is a set of connected points bounding the disc (plane area).
+// Judging from my own observations, the circle seems round to the eye when its resolution is greater than or equal to 24.
+#define RESOLUTION_OF_CIRCLE 28u
+
 #include "vezbe_03_zadatak_03.h"
 
 int window_width = 500;
@@ -41,6 +46,9 @@ int draw_vezbe_03_zadatak_03()
 		return 3;
 	}
 
+	// Set the width of lines to 7 pixels.
+	glLineWidth(7.0f);
+
 	ShaderProgram shaderProgram("Vezbe_03/Zadatak_03/vertex_shader_for_03_03.glsl", 
 		"Vezbe_03/Zadatak_03/fragment_shader_for_03_03.glsl");
 	if (shaderProgram.errorCode)
@@ -56,32 +64,64 @@ int draw_vezbe_03_zadatak_03()
 		// change in face orientation, OpenGL will automatically change the order of drawing so the user doesn't have to
 		// worry about that.
 		// position   // color
-		 1.0f,  1.0f, 0.0f, 1.0f, 1.0f, 
-		 0.7f,  0.7f, 0.5f, 0.0f, 0.5f, 
-		 1.0f, -1.0f, 1.0f, 1.0f, 0.0f, 
-		 0.7f, -0.7f, 0.5f, 0.2f, 1.0f, 
-		-1.0f, -1.0f, 1.0f, 0.0f, 0.0f, 
-		-0.7f, -0.7f, 0.0f, 0.5f, 0.5f, 
-		-1.0f,  1.0f, 0.5f, 0.1f, 0.5f, 
-		-0.7f,  0.7f, 0.5f, 0.0f, 0.5f, 
-		 1.0f,  1.0f, 0.0f, 1.0f, 1.0f, 
-		 0.7f,  0.7f, 0.5f, 0.0f, 0.5f
+		 1.0f,  1.0f, 0.0f, 1.0f, 1.0f, // v0
+		 0.7f,  0.7f, 0.5f, 0.0f, 0.5f, // v1
+		 1.0f, -1.0f, 1.0f, 1.0f, 0.0f, // v2
+		 0.7f, -0.7f, 0.5f, 0.2f, 1.0f, // v3
+		-1.0f, -1.0f, 1.0f, 0.0f, 0.0f, // v4
+		-0.7f, -0.7f, 0.0f, 0.5f, 0.5f, // v5
+		-1.0f,  1.0f, 0.5f, 0.1f, 0.5f, // v6
+		-0.7f,  0.7f, 0.5f, 0.0f, 0.5f, // v7
+		 1.0f,  1.0f, 0.0f, 1.0f, 1.0f, // v8
+		 0.7f,  0.7f, 0.5f, 0.0f, 0.5f  // v9
 	};
-	int strideOfVertexOfBoundingStripe = 5 * sizeof(float);
 
-	unsigned int boundingStripeVAO;
+	// I only need the right half of the circle, a.k.a. its fourth and first quadrant.
+	const unsigned int sizeOfArrayOfVerticesOfLetterD = 
+		NUMBER_OF_DATA_IN_VERTEX_OF_CIRCLE * (RESOLUTION_OF_CIRCLE / 2u + 1u);
+	float verticesOfLetterD[sizeOfArrayOfVerticesOfLetterD];
+	// Set the radius of the circle.
+	float r = 0.35f;
+	for (int i = -static_cast<int>(RESOLUTION_OF_CIRCLE) / 4; i <= static_cast<int>(RESOLUTION_OF_CIRCLE) / 4; i++)
+	{
+		unsigned int locationInArrayOfVertices = static_cast<unsigned int>(i + RESOLUTION_OF_CIRCLE / 4);
+
+		// position
+		verticesOfLetterD[NUMBER_OF_DATA_IN_VERTEX_OF_CIRCLE * locationInArrayOfVertices] = 
+			r * cos((3.141592f / 180.0f) * (i * (360.0f / RESOLUTION_OF_CIRCLE))) - r / 2.0f;
+		verticesOfLetterD[NUMBER_OF_DATA_IN_VERTEX_OF_CIRCLE * locationInArrayOfVertices + 1u] = 
+			r * sin((3.141592f / 180.0f) * (i * (360.0f / RESOLUTION_OF_CIRCLE)));
+		// color
+		verticesOfLetterD[NUMBER_OF_DATA_IN_VERTEX_OF_CIRCLE * locationInArrayOfVertices + 2u] = 0.0f;
+		verticesOfLetterD[NUMBER_OF_DATA_IN_VERTEX_OF_CIRCLE * locationInArrayOfVertices + 3u] = 1.0f;
+		verticesOfLetterD[NUMBER_OF_DATA_IN_VERTEX_OF_CIRCLE * locationInArrayOfVertices + 4u] = 0.0f;
+	}
+
+	unsigned int boundingStripeVAO, letterDVAO;
 	glGenVertexArrays(1, &boundingStripeVAO);
-	unsigned int boundingStripeVBO;
+	glGenVertexArrays(1, &letterDVAO);
+	unsigned int boundingStripeVBO, letterDVBO;
 	glGenBuffers(1, &boundingStripeVBO);
+	glGenBuffers(1, &letterDVBO);
 
 	glBindVertexArray(boundingStripeVAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, boundingStripeVBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(verticesOfBoundingStripe), verticesOfBoundingStripe, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0u, 2, GL_FLOAT, GL_FALSE, strideOfVertexOfBoundingStripe, (void*) 0);
+	glVertexAttribPointer(0u, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*) 0);
 	glEnableVertexAttribArray(0u);
-	glVertexAttribPointer(1u, 3, GL_FLOAT, GL_FALSE, strideOfVertexOfBoundingStripe, (void*) (2 * sizeof(float)));
+	glVertexAttribPointer(1u, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*) (2 * sizeof(float)));
+	glEnableVertexAttribArray(1u);
+
+	glBindVertexArray(letterDVAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, letterDVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verticesOfLetterD), verticesOfLetterD, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0u, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*) 0);
+	glEnableVertexAttribArray(0u);
+	glVertexAttribPointer(1u, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*) (2 * sizeof(float)));
 	glEnableVertexAttribArray(1u);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0u);
@@ -97,11 +137,19 @@ int draw_vezbe_03_zadatak_03()
 		glClearColor(colorOfBackground, colorOfBackground, colorOfBackground, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 		glBindVertexArray(boundingStripeVAO);
 		// Parameters: primitive, index of first vertex to be drawn, total number of vertices to be drawn.
-		glDrawArrays(GL_TRIANGLE_STRIP, 0, sizeof(verticesOfBoundingStripe) / strideOfVertexOfBoundingStripe);
+		glDrawArrays(GL_TRIANGLE_STRIP, 0, sizeof(verticesOfBoundingStripe) / (5 * sizeof(float)));
 		// We draw 10 vertices, which is the quotient (the result of the division) of the size of bounding stripe's
 		// vertices array and the size of a single vertex in it.
+
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		glBindVertexArray(letterDVAO);
+		// Parameters: primitive, index of first vertex to be drawn, total number of vertices to be drawn.
+		glDrawArrays(GL_LINE_LOOP, 0, sizeof(verticesOfLetterD) / (5 * sizeof(float)));
+		// We draw RESOLUTION_OF_CIRCLE / 2 + 1 vertices, which is the quotient (the result of the division) of the size of
+		// letter "D"'s vertices array and the number of data in a vertex.
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
