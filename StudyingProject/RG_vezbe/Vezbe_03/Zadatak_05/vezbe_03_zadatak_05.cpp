9dee31f5d3552b43cf1e73e39a6978676f0e5aa2
Darijan Mićić <darijan.micic10@gmail.com>
2024-11-02 23:28:43 +0100

    feat: draw red point & wasd move it (practice 3.5)
    Zadatak 5
    Napisati aplikaciju čiji je ekranski prostor ceo ekran i koja prikazuje crvenu tačku na centru ekrana. Tačka je veća od 1
    piksela i može se pomerati po ekranu pomoću WASD tastera, a ukoliko izađe izvan ekrana, pojavljuje se na suprotnoj ivici.
    Boja tačke pulsira od crvene do žute tokom vremena, i pulsira brže što je tačka dalja od koordinatnog početka.
    Tačka ostavlja trag trenutne boje za sobom.
    Taster ESC zatvara program. Taster R briše ekran i resetuje tačku na koordinatni početak.

--- StudyingProject/RG_vezbe/Vezbe_03/Zadatak_05/vezbe_03_zadatak_05.cpp
+++ StudyingProject/RG_vezbe/Vezbe_03/Zadatak_05/vezbe_03_zadatak_05.cpp
@@ -1,8 +1,23 @@
 #include "vezbe_03_zadatak_05.h"
 
-const int window_width = 800;
-const int window_height = 600;
+int window_width_for_03_05 = 800;
+int window_height_for_03_05 = 600;
 
+float pointSize_for_03_05 = 7.0f;
+
+float movementOfPointOnXAxis_for_03_05 = 0.0f;
+float movementOfPointOnYAxis_for_03_05 = 0.0f;
+
+float deltaTime_for_03_05 = 0.0f;
+float previousFrameTime_for_03_05 = 0.0f;
+
+/* Zadatak 5
+Napisati aplikaciju čiji je ekranski prostor ceo ekran i koja prikazuje crvenu tačku na centru ekrana. Tačka je veća od 1
+piksela i može se pomerati po ekranu pomoću WASD tastera, a ukoliko izađe izvan ekrana, pojavljuje se na suprotnoj ivici.
+Boja tačke pulsira od crvene do žute tokom vremena, i pulsira brže što je tačka dalja od koordinatnog početka.
+Tačka ostavlja trag trenutne boje za sobom.
+Taster ESC zatvara program. Taster R briše ekran i resetuje tačku na koordinatni početak.
+*/
 int draw_vezbe_03_zadatak_05()
 {
 	if (glfwInit() != GLFW_TRUE)
@@ -15,8 +30,12 @@ int draw_vezbe_03_zadatak_05()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(window_width, window_height, 
-		"Ve�be 3 - zadatak 5", NULL, NULL);
+	/*
+	GLFWwindow* window = glfwCreateWindow(window_width_for_03_05, window_height_for_03_05, 
+		"Vežbe 3 - zadatak 5", glfwGetPrimaryMonitor(), NULL);
+	*/
+	GLFWwindow* window = glfwCreateWindow(window_width_for_03_05, window_height_for_03_05, 
+		"Vežbe 3 - zadatak 5", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Window was not created!" << std::endl;
@@ -36,6 +55,11 @@ int draw_vezbe_03_zadatak_05()
 		return 3;
 	}
 
+	// Configure global OpenGL state.
+	// Enable programmable point size.
+	glEnable(GL_PROGRAM_POINT_SIZE);
+	glPointSize(pointSize_for_03_05);
+
 	ShaderProgram shaderProgram("Vezbe_03/Zadatak_05/vertex_shader_for_03_05.glsl", 
 		"Vezbe_03/Zadatak_05/fragment_shader_for_03_05.glsl");
 	if (shaderProgram.errorCode)
@@ -45,38 +69,48 @@ int draw_vezbe_03_zadatak_05()
 		return shaderProgram.errorCode;
 	}
 
-	float vertices[] = {
-		0.0f, 0.0f, 0.0f
+	float verticesOfPoint[] = {
+		// position   // color
+		 0.0f,  0.0f, 1.0f, 0.0f, 0.0f
 	};
 
-	unsigned int VAO;
-	glGenVertexArrays(1, &VAO);
-	unsigned int VBO;
-	glGenBuffers(1, &VBO);
+	unsigned int pointVAO;
+	glGenVertexArrays(1, &pointVAO);
+	unsigned int pointVBO;
+	glGenBuffers(1, &pointVBO);
 
-	glBindVertexArray(VAO);
+	glBindVertexArray(pointVAO);
 
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, pointVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verticesOfPoint), verticesOfPoint, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0u, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) 0);
+	glVertexAttribPointer(0u, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*) 0);
 	glEnableVertexAttribArray(0u);
+	glVertexAttribPointer(1u, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*) (2 * sizeof(float)));
+	glEnableVertexAttribArray(1u);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0u);
 	glBindVertexArray(0u);
-
-	glPointSize(7.5f);
 
 	shaderProgram.useProgram();
 
 	while (!glfwWindowShouldClose(window))
 	{
+		float currentFrameTime = static_cast<float>(glfwGetTime());
+		deltaTime_for_03_05 = currentFrameTime - previousFrameTime_for_03_05;
+		previousFrameTime_for_03_05 = currentFrameTime;
+
 		processInput_for_vezbe_03_zadatak_05(window);
 
 		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glBindVertexArray(VAO);
+		// Update point position uniforms.
+		shaderProgram.setFloatUniform("movementOfPointOnXAxis", movementOfPointOnXAxis_for_03_05);
+		shaderProgram.setFloatUniform("movementOfPointOnYAxis", movementOfPointOnYAxis_for_03_05);
+
+		glBindVertexArray(pointVAO);
+		// Parameters: primitive, index of first vertex to be drawn, total number of vertices to be drawn.
 		glDrawArrays(GL_POINTS, 0, 1);
 
 		glfwSwapBuffers(window);
@@ -90,6 +124,9 @@ int draw_vezbe_03_zadatak_05()
 
 void framebuffer_size_callback_for_vezbe_03_zadatak_05(GLFWwindow* window, int width, int height)
 {
+	window_width_for_03_05 = width;
+	window_height_for_03_05 = height;
+
 	glViewport(0, 0, width, height);
 }
 
@@ -98,5 +135,51 @@ void processInput_for_vezbe_03_zadatak_05(GLFWwindow* window)
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 	{
 		glfwSetWindowShouldClose(window, true);
+	}
+
+	float movementSpeed = 0.75f * deltaTime_for_03_05;
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+	{
+		movementOfPointOnYAxis_for_03_05 += movementSpeed;
+
+		// Make the point emerge on the opposite side of the screen space when the screen space's boundaries are crossed.
+		if (movementOfPointOnYAxis_for_03_05 > 1.0f)
+		{
+			movementOfPointOnYAxis_for_03_05 = -1.0f;
+		}
+	}
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+	{
+		movementOfPointOnYAxis_for_03_05 -= movementSpeed;
+
+		// Make the point emerge on the opposite side of the screen space when the screen space's boundaries are crossed.
+		if (movementOfPointOnYAxis_for_03_05 < -1.0f)
+		{
+			movementOfPointOnYAxis_for_03_05 = 1.0f;
+		}
+	}
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+	{
+		movementOfPointOnXAxis_for_03_05 -= movementSpeed;
+		// Make the point emerge on the opposite side of the screen space when the screen space's boundaries are crossed.
+		if (movementOfPointOnXAxis_for_03_05 < -1.0f)
+		{
+			movementOfPointOnXAxis_for_03_05 = 1.0f;
+		}
+	}
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+	{
+		movementOfPointOnXAxis_for_03_05 += movementSpeed;
+
+		// Make the point emerge on the opposite side of the screen space when the screen space's boundaries are crossed.
+		if (movementOfPointOnXAxis_for_03_05 > 1.0f)
+		{
+			movementOfPointOnXAxis_for_03_05 = -1.0f;
+		}
+	}
+	if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS)
+	{
+		movementOfPointOnXAxis_for_03_05 = 0.0f;
+		movementOfPointOnYAxis_for_03_05 = 0.0f;
 	}
 }
