28bb65715fa2abb46fb42874c749c5b7446b9418
Darijan Mićić <darijan.micic10@gmail.com>
2024-11-03 22:52:46 +0100

    feat: paint triangle as stripes (practice 3.6)
    Zadatak 6
    Nacrtati beli trougao na crnoj pozadini u gornjem levom kvadrantu ekrana, koji se pomera po ekranu sa WASD tasterima, a vraća na koordinatni početak tasterom R. Deo trougla koji pređe u gornji desni kvadrant biva obojen crvenom bojom, deo koji pređe u donji desni dio biva obojen ljubičastom bojom, a deo koji pređe u levi donji kvadrant biva obojen plavom bojom.
    Deo trougla koji pređe na donju polovinu ekrana se crta u vertikalnim prugama. Ukoliko trougao priđe donjem desnom uglu, ili se pritisne taster ESC, program se zatvara.

--- StudyingProject/RG_vezbe/Vezbe_03/Zadatak_06/fragment_shader_for_03_06.glsl
+++ StudyingProject/RG_vezbe/Vezbe_03/Zadatak_06/fragment_shader_for_03_06.glsl
@@ -8,20 +8,38 @@ uniform int windowHeight;
 void main()
 {
 	vec3 colorOfTriangle = vec3(1.0f);
-	// 1. quadrant: paint the fragment in red.
+	// 1. quadrant: paint the fragment with red color.
 	if (gl_FragCoord.x > windowWidth / 2 && gl_FragCoord.y > windowHeight / 2)
 	{
 		colorOfTriangle = vec3(1.0f, 0.0f, 0.0f);
 	}
-	// 3. quadrant: paint the fragment in blue.
-	if (gl_FragCoord.x < windowWidth / 2 && gl_FragCoord.y < windowHeight / 2)
+	// Bottom half of the screen: paint as vertical stripes (I chose for those stripes to be painted with yellow color).
+	else if (gl_FragCoord.y < windowHeight / 2)
 	{
-		colorOfTriangle = vec3(0.0f, 0.0f, 1.0f);
-	}
-	// 4. quadrant: paint the fragment in purple.
-	if (gl_FragCoord.x > windowWidth / 2 && gl_FragCoord.y < windowHeight / 2)
-	{
-		colorOfTriangle = vec3(1.0f, 0.0f, 1.0f);
+		// 3. quadrant: paint the fragment with blue color.
+		if (gl_FragCoord.x < windowWidth / 2)
+		{
+			if (mod(gl_FragCoord.x, 15) < 7)
+			{
+				colorOfTriangle = vec3(0.0f, 0.0f, 1.0f);
+			}
+			else
+			{
+				colorOfTriangle = vec3(1.0f, 1.0f, 0.0f);
+			}
+		}
+		// 4. quadrant: paint the fragment with purple color.
+		else
+		{
+			if (mod(gl_FragCoord.x, 15) < 7)
+			{
+				colorOfTriangle = vec3(1.0f, 0.0f, 1.0f);
+			}
+			else
+			{
+				colorOfTriangle = vec3(1.0f, 1.0f, 0.0f);
+			}
+		}
 	}
 
 	FragColor = vec4(colorOfTriangle, 1.0f);
