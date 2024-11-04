2363b5429040e27a4a8e4e2ee85da21823d5c222
Darijan Mićić <darijan.micic10@gmail.com>
2024-11-04 19:59:58 +0100

    feat: move triangles in circles (practice 3.7)
    Zadatak 7
    Nacrtati dva šarena trougla, jedan iznad x-ose, a drugi ispod x-ose. Trouglovi se tokom vremena kreću po nevidljivoj kružnici dovoljno malog poluprečnika tako da ne prelaze x-osu. Trougao iznad x-ose se kreće u smeru kazaljke na časovniku, dok se trougao ispod x-ose kreće u suprotnom smeru. Boja trouglova postaje svetlija što su oni dalji od x-ose, odnosno tamnija što su bliži x-osi.

--- StudyingProject/RG_vezbe/Vezbe_03/Zadatak_07/vertex_shader_for_03_07.glsl
+++ StudyingProject/RG_vezbe/Vezbe_03/Zadatak_07/vertex_shader_for_03_07.glsl
@@ -5,9 +5,25 @@ layout (location = 1) in vec3 aColor;
 
 out vec3 Color;
 
+uniform float movementOfTrianglesOnXAxis;
+uniform float movementOfTrianglesOnYAxis;
+
 void main()
 {
 	Color = aColor;
 
-	gl_Position = vec4(aPos, 0.0f, 1.0f);
+	// Upper triangle (above the x-axis): move clockwise.
+	if (aPos.y > 0.0f)
+	{
+		float positionOnXAxis = aPos.x - movementOfTrianglesOnXAxis;
+		float positionOnYAxis = aPos.y + movementOfTrianglesOnYAxis;
+		gl_Position = vec4(positionOnXAxis, positionOnYAxis, 0.0f, 1.0f);
+	}
+	// Lower triangle (below the x-axis): move anti-clockwise.
+	else
+	{
+		float positionOnXAxis = aPos.x - movementOfTrianglesOnXAxis;
+		float positionOnYAxis = aPos.y - movementOfTrianglesOnYAxis;
+		gl_Position = vec4(positionOnXAxis, positionOnYAxis, 0.0f, 1.0f);
+	}
 }
--- StudyingProject/RG_vezbe/Vezbe_03/Zadatak_07/vezbe_03_zadatak_07.cpp
+++ StudyingProject/RG_vezbe/Vezbe_03/Zadatak_07/vezbe_03_zadatak_07.cpp
@@ -3,7 +3,7 @@
 int window_width_for_03_07 = 800;
 int window_height_for_03_07 = 600;
 
-const float triangleSideLength_for_03_07 = 0.1f;
+const float triangleSideLength_for_03_07 = 0.3f;
 
 /* Zadatak 7
 Nacrtati dva šarena trougla, jedan iznad x-ose, a drugi ispod x-ose. Trouglovi se tokom vremena kreću po nevidljivoj
@@ -54,15 +54,15 @@ int draw_vezbe_03_zadatak_07()
 	}
 
 	float vertices[] = {
-		// position                                                          // color
+		// position                                                                 // color
 		// first triangle, set to be above the x-axis
-		-triangleSideLength_for_03_07,  0.5f - triangleSideLength_for_03_07, 1.0f, 0.0f, 0.0f, 
-		 triangleSideLength_for_03_07,  0.5f - triangleSideLength_for_03_07, 0.0f, 1.0f, 0.0f, 
-		 0.0f,                          0.5f + triangleSideLength_for_03_07, 0.0f, 0.0f, 1.0f, 
+		-triangleSideLength_for_03_07,  triangleSideLength_for_03_07,               1.0f, 0.0f, 0.0f, 
+		 triangleSideLength_for_03_07,  triangleSideLength_for_03_07,               0.0f, 1.0f, 0.0f, 
+		 0.0f,                          triangleSideLength_for_03_07 * 2.0f,        0.0f, 0.0f, 1.0f, 
 		// second triangle, set to be below the x-axis
-		-triangleSideLength_for_03_07, -0.5f + triangleSideLength_for_03_07, 1.0f, 0.0f, 0.0f, 
-		 triangleSideLength_for_03_07, -0.5f + triangleSideLength_for_03_07, 0.0f, 1.0f, 0.0f, 
-		 0.0f,                         -0.5f - triangleSideLength_for_03_07, 0.0f, 0.0f, 1.0f
+		-triangleSideLength_for_03_07, -1.0f + triangleSideLength_for_03_07,        1.0f, 0.0f, 0.0f, 
+		 triangleSideLength_for_03_07, -1.0f + triangleSideLength_for_03_07,        0.0f, 1.0f, 0.0f, 
+		 0.0f,                         -1.0f + triangleSideLength_for_03_07 * 2.0f, 0.0f, 0.0f, 1.0f
 	};
 
 	unsigned int VAO;
@@ -85,12 +85,25 @@ int draw_vezbe_03_zadatak_07()
 
 	shaderProgram.useProgram();
 
+	float movementOfTrianglesOnXAxis = 0.0f;
+	float movementOfTrianglesOnYAxis = 0.0f;
+	// Radius of the invinsible circle the triangles will move on.
+	// It has to be smaller than the minimal absolute y-coordinate of all vertices.
+	float r = 0.25f;
 	while (!glfwWindowShouldClose(window))
 	{
 		processInput_for_vezbe_03_zadatak_07(window);
 
 		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		float time = static_cast<float>(glfwGetTime());
+		movementOfTrianglesOnXAxis = r * cos(time);
+		movementOfTrianglesOnYAxis = r * sin(time);
+
+		// Update triangles position uniforms.
+		shaderProgram.setFloatUniform("movementOfTrianglesOnXAxis", movementOfTrianglesOnXAxis);
+		shaderProgram.setFloatUniform("movementOfTrianglesOnYAxis", movementOfTrianglesOnYAxis);
 
 		glBindVertexArray(VAO);
 		// Parameters: primitive, index of first vertex to be drawn, total number of vertices to be drawn.
