a4bda095180f595674ca864c7ce79026aca28c1d
Darijan Mićić <darijan.micic10@gmail.com>
2024-11-04 15:25:42 +0100

    feat: draw 2 triangles > & < x-axis (practice 3.7)
    Zadatak 7
    Nacrtati dva šarena trougla, jedan iznad x-ose, a drugi ispod x-ose. Trouglovi se tokom vremena kreću po nevidljivoj kružnici dovoljno malog poluprečnika tako da ne prelaze x-osu. Trougao iznad x-ose se kreće u smeru kazaljke na časovniku, dok se trougao ispod x-ose kreće u suprotnom smeru. Boja trouglova postaje svetlija što su oni dalji od x-ose, odnosno tamnija što su bliži x-osi.

--- StudyingProject/RG_vezbe/Vezbe_03/Zadatak_07/vertex_shader_for_03_07.glsl
+++ StudyingProject/RG_vezbe/Vezbe_03/Zadatak_07/vertex_shader_for_03_07.glsl
@@ -1,8 +1,13 @@
 #version 330 core
 
-layout (location = 0) in vec3 aPos;
+layout (location = 0) in vec2 aPos;
+layout (location = 1) in vec3 aColor;
+
+out vec3 Color;
 
 void main()
 {
-	gl_Position = vec4(aPos, 1.0f);
+	Color = aColor;
+
+	gl_Position = vec4(aPos, 0.0f, 1.0f);
 }
--- StudyingProject/RG_vezbe/Vezbe_03/Zadatak_07/vezbe_03_zadatak_07.cpp
+++ StudyingProject/RG_vezbe/Vezbe_03/Zadatak_07/vezbe_03_zadatak_07.cpp
@@ -1,7 +1,9 @@
 #include "vezbe_03_zadatak_07.h"
 
-const int window_width = 800;
-const int window_height = 600;
+int window_width_for_03_07 = 800;
+int window_height_for_03_07 = 600;
+
+const float triangleSideLength_for_03_07 = 0.1f;
 
 /* Zadatak 7
 Nacrtati dva šarena trougla, jedan iznad x-ose, a drugi ispod x-ose. Trouglovi se tokom vremena kreću po nevidljivoj
@@ -21,7 +23,7 @@ int draw_vezbe_03_zadatak_07()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(window_width, window_height, 
+	GLFWwindow* window = glfwCreateWindow(window_width_for_03_07, window_height_for_03_07, 
 		"Vežbe 3 - zadatak 7", NULL, NULL);
 	if (window == NULL)
 	{
@@ -52,7 +54,15 @@ int draw_vezbe_03_zadatak_07()
 	}
 
 	float vertices[] = {
-		0.0f, 0.0f, 0.0f
+		// position                                                          // color
+		// first triangle, set to be above the x-axis
+		-triangleSideLength_for_03_07,  0.5f - triangleSideLength_for_03_07, 1.0f, 0.0f, 0.0f, 
+		 triangleSideLength_for_03_07,  0.5f - triangleSideLength_for_03_07, 0.0f, 1.0f, 0.0f, 
+		 0.0f,                          0.5f + triangleSideLength_for_03_07, 0.0f, 0.0f, 1.0f, 
+		// second triangle, set to be below the x-axis
+		-triangleSideLength_for_03_07, -0.5f + triangleSideLength_for_03_07, 1.0f, 0.0f, 0.0f, 
+		 triangleSideLength_for_03_07, -0.5f + triangleSideLength_for_03_07, 0.0f, 1.0f, 0.0f, 
+		 0.0f,                         -0.5f - triangleSideLength_for_03_07, 0.0f, 0.0f, 1.0f
 	};
 
 	unsigned int VAO;
@@ -65,13 +75,13 @@ int draw_vezbe_03_zadatak_07()
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0u, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) 0);
+	glVertexAttribPointer(0u, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*) 0);
 	glEnableVertexAttribArray(0u);
+	glVertexAttribPointer(1u, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*) (2 * sizeof(float)));
+	glEnableVertexAttribArray(1u);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0u);
 	glBindVertexArray(0u);
-
-	glPointSize(7.5f);
 
 	shaderProgram.useProgram();
 
@@ -83,7 +93,8 @@ int draw_vezbe_03_zadatak_07()
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_POINTS, 0, 1);
+		// Parameters: primitive, index of first vertex to be drawn, total number of vertices to be drawn.
+		glDrawArrays(GL_TRIANGLES, 0, 6);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -96,6 +107,9 @@ int draw_vezbe_03_zadatak_07()
 
 void framebuffer_size_callback_for_vezbe_03_zadatak_07(GLFWwindow* window, int width, int height)
 {
+	window_width_for_03_07 = width;
+	window_height_for_03_07 = height;
+
 	glViewport(0, 0, width, height);
 }
 
